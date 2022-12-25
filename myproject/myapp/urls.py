e2b98d2eb8cd11d017158badf8b2de81909c0c45
Sam <mrsambaga@gmail.com>
2022-12-26 01:52:34 +0700

    modify urls & views with drinks

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = {
     path('', views.home, name="home"),
+    path('drinks/<str:drink_name>', views.drinks, name='drink_name'),
 }
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,3 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Welcome to Little Lemon")
+
+def drinks(request, drink_name):
+    drink = {
+        'mocha' : 'type of coffee',
+        'tea' : 'type of beverage',
+        'lemonade' : 'type of refreshment'
+    }
+
+    choice_of_drink = drink[drink_name]
+
+    return HttpResponse(f"<h2> {drink_name} </h2>" + choice_of_drink)
