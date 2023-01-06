2f54c793ac472542f726b809692b9c22570ed5e6
Sam <m.r.sambaga@gmail.com>
2023-01-07 03:30:47 +0700

    add base template & implement template inheritence

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('home/', views.home, name="home"),
     path('about/', views.about, name="about"),
     path('menu/', views.menu, name="menu"),
-    path('booking/', views.form_view, name="booking"),
+    path('book/', views.book, name="book"),
     path('drinks/<str:drink_name>', views.drinks, name='drink_name'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -5,13 +5,11 @@ from myapp.models import Menu
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome to Little Lemon")
-
-def about(request):
-    return HttpResponse("About us")
+    return render(request, 'home.html')
 
 def book(request):
-    return HttpResponse("Make a booking")
+    return render(request, 'book.html')
+
 
 def drinks(request, drink_name):
     drink = {
@@ -34,10 +32,10 @@ def form_view(request):
     return render(request, "booking.html", context)
 
 def about(request):
-    about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."}
-    return render(request, 'about.html', {'content':about_content})
+    return render(request, 'about.html')
 
 def menu(request):
     menu_items = Menu.objects.all()
     item_dict = {'menu':menu_items}
     return render(request, "menu.html", item_dict)
+
