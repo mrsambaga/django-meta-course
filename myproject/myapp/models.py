3fbb13ce9e3c251297fe4c4562315277e7a051c5
Sam <m.r.sambaga@gmail.com>
2023-01-07 01:35:43 +0700

    add menu template

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -23,3 +23,10 @@ class Employees(models.Model):
     shift = models.IntegerField()
     def __str__(self) -> str:
         return self.first_name
+
+class Menu(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from myapp.forms import BookingForm
+from myapp.models import Menu
 
 # Create your views here.
 def home(request):
@@ -8,9 +9,6 @@ def home(request):
 
 def about(request):
     return HttpResponse("About us")
-
-def menu(request):
-    return HttpResponse("Menu")
 
 def book(request):
     return HttpResponse("Make a booking")
@@ -40,5 +38,6 @@ def about(request):
     return render(request, 'about.html', {'content':about_content})
 
 def menu(request):
-    about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."} 
-    return render(request, "menu.html", {'content': about_content})
+    menu_items = Menu.objects.all()
+    item_dict = {'menu':menu_items}
+    return render(request, "menu.html", item_dict)
