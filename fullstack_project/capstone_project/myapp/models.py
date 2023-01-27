27be1dc9edd50d8d9d45c565a8a26343a149cd78
Sam <m.r.sambaga@gmail.com>
2023-01-28 00:18:11 +0700

    setup initial api

--- fullstack_project/capstone_project/myapp/models.py
+++ fullstack_project/capstone_project/myapp/models.py
@@ -9,3 +9,12 @@ class Booking(models.Model):
 
     def __str__(self): 
         return self.first_name
+
+# Add code to create Menu model
+class Menu(models.Model):
+   name = models.CharField(max_length=200) 
+   price = models.IntegerField(null=False) 
+   menu_item_description = models.TextField(max_length=1000, default='') 
+
+   def __str__(self):
+      return self.name
