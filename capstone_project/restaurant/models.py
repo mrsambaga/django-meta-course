cea68ecd23daba3d21f80a5e2ee1029ac9926a10
Sam <m.r.sambaga@gmail.com>
2023-02-09 02:21:11 +0700

    create menu & book model

--- capstone_project/restaurant/models.py
+++ capstone_project/restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    guest = models.IntegerField(max_length=6)
+    booking = models.DateField()
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    
